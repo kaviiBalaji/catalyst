2cc9e4f31fcbb9fc18926e52b400ab0db45bd0b6
BAK-User5 <kavibalaji.m@vinsinfo.com>
2025-04-22 15:39:49 +0530

    update

--- demo-app/core/components/promotionBanner/promotionBanner.tsx
+++ demo-app/core/components/promotionBanner/promotionBanner.tsx
@@ -1,36 +1,23 @@
 'use client';
 
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "./PromotionBanner.css"
 
 export default function PromotionBanner() {
     let promotions: string[] = [
         'Buy more, save more—don`t miss out on our tiered discounts!',
         'Big savings just for you! Use code [Pineapple 2KG] at checkout and save!'
     ];
-    
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: false,
-    };
-    
+
     return (
         <div className="promotion-banner">
-            <Slider {...settings}>
+            <div className="promotion-slider">
                 {promotions.map((value, index) => (
-                    <div key={index}>
+                    <div key={index} className="promotion-text">
                         <span>{value}</span>
                     </div>
                 ))}
-            </Slider>
+            </div>
         </div>
     );
 }
